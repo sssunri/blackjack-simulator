a1896b5c51e7caf8a6feb7fff1afe6b2da231b05
Chin Chin <sssunri@icloud.com>
2024-08-23 10:01:39 +0800

    add dealer logic and winner determination

--- Blackjack.java
+++ Blackjack.java
@@ -8,11 +8,23 @@ public class Blackjack {
     public static void main(String[] args) {
         System.out.println("Welcome to Java Casino!");
 
-        playGame();
+        int handValue = playGame();
+        if (handValue == -1) {
+            System.out.println("\nDealer wins!");
+            return;
+        }
+
+        int dealerHand = dealerPlays();
+        if (dealerHand == -1) {
+            System.out.println("\nPlayer wins!");
+            return;
+        }
+
+        determineWinner(handValue, dealerHand);
     }
 
     // manages the main game loop, allowing the player to draw cards and track hand value
-    public static void playGame() {
+    public static int playGame() {
         int handValue = 0;
 
         char response = askToDrawCard();
@@ -23,12 +35,45 @@ public class Blackjack {
 
             if (handValue > 21) {
                 System.out.println("Your total is " + handValue + ". You've busted!");
-                break;
+                return -1;
             } else {
                 System.out.println("Your current total is: " + handValue);
             }
 
             response = askToDrawCard();
+        }
+
+        return handValue;
+    }
+
+    public static int dealerPlays() {
+        System.out.println("\nDealer's turn.\n");
+
+        int dealerHand = 0;
+        int card;
+
+        while (dealerHand < 17) {
+            card = drawCard();
+            dealerHand += card;
+
+            if (dealerHand > 21) {
+                System.out.println("Dealer's total is " + dealerHand + ". He'd busted!");
+                return -1;
+            } else {
+                System.out.println("Dealer's current total is: " + dealerHand);
+            }
+        }
+
+        return dealerHand;
+    }
+
+    public static void determineWinner(int handValue, int dealerHand) {
+        if (handValue > dealerHand) {
+            System.out.println("\nPlayer wins!");
+        } else if (dealerHand > handValue) {
+            System.out.println("\nDealer wins!");
+        } else {
+            System.out.println("\nIt's a tie!");
         }
     }
 
